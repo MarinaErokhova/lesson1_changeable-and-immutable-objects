5cb19dbf177bf12d8487b327bc2b979ba10affb9
MarinaErokhova <erokhovamn@gmail.com>
2024-09-14 16:59:07 +0300

    Add files via upload

---
+++ module_1_5_changeable and immutable objects.py
@@ -0,0 +1,8 @@
+immutable_var = 'Marina', 28, 62.5
+print(immutable_var, type(immutable_var))
+#immutable_var[1] = 'Erokhova' #Объект 'tuple' не поддерживает присвоение элементов
+#print(immutable_var)
+mutable_list = [1,2,3,5,7,11]
+print(mutable_list)
+mutable_list = ['cat', 'dog', 'parrot', 62.5]
+print(mutable_list)
